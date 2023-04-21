eb8c6e9d1043d0db9f6fbc118402585afe866aa3
hasnalhocine <ahassna-star@hotmail.com>
2023-04-20 17:38:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
