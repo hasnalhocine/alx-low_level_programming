e204b1a0e5a87486068bcedf84d87cda5501e284
hasnalhocine <ahassna-star@hotmail.com>
2023-04-23 05:44:47 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,34 +3,37 @@
 #include <time.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - Entry point
  * Return: Always 0 (success)
  */
 
 int main(void)
 
 {
-	int n, m, l,
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+
+	for (m = 49; m < 58; m++)
+	{
+
+	for (l = 50; l < 58; l++)
+	{
+
+	if (l > m && m > n)
+	{
+	putchar(n);
+	putchar(m);
+	putchar(l);
+	if (n != 55 || m != 56)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
