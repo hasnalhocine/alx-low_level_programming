9bebc3949ead4398b7c4a0d24073e579099c1e86
hasnalhocine <ahassna-star@hotmail.com>
2023-04-26 11:07:13 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 {
 
 	int i = 0;
-	long j = l, k = 2;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
 	if (i == 0)
 	printf("%ld", j);
-	else if (i == l)
+	else if (i == 1)
 	printf(", %ld", k);
 	else
 	{
