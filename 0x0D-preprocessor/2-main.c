b61a47262fec0f1b89c52bd016889f563c481388
hasnalhocine <ahassna-star@hotmail.com>
2023-05-22 05:16:50 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return (0)
+ */
+
+int main(void)
+
+{
+	printf("name of the file: %s\n", __FILE__);
+
+	return (0);
+}
